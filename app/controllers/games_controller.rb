b1eb6381bc8501de8ad10afc2cb4886b641f1f88
Amelie Boudier <amelie.boudier01@gmail.com>
2021-01-04 17:39:18 +0100

    added players creation to create action of game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     if @game.save
+      create_players
       redirect_to game_path(@game)
     else
       render 'pages/home'
@@ -10,11 +11,23 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    @players = @game.players
   end
 
   private
 
+  def create_players
+    Player.create(name: player_params[:player_1][:name], game: @game)
+    Player.create(name: player_params[:player_2][:name], game: @game)
+    Player.create(name: player_params[:player_3][:name], game: @game)
+    Player.create(name: player_params[:player_4][:name], game: @game)
+  end
+
   def game_params
-    params.require(:game).permit(:sets, :legs, :type)
+    params.require(:game).permit(:sets, :legs, :game_type)
+  end
+
+  def player_params
+    params.require(:game).permit(player_1: [:name], player_2: [:name], player_3: [:name], player_4: [:name])
   end
 end
