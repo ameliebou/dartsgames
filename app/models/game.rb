96ca3cf8429bd4d5871d02d86d50b9a1f7837ae1
Amelie Boudier <amelie.boudier01@gmail.com>
2021-01-04 14:08:02 +0100

    added set_password & self.types methods to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,16 @@
+require 'securerandom'
+
 class Game < ApplicationRecord
   has_many :players
+  before_create :set_password
+
+  private
+
+  def self.types
+    [101, 301, 501, 701, 1001]
+  end
+
+  def set_password
+    self.password = SecureRandom.hex(4)
+  end
 end
