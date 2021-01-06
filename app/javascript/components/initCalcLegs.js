229ac09d4ed55d625956c7384a716b67d6ae8827
Amelie Boudier <amelie.boudier01@gmail.com>
2021-01-06 16:51:10 +0100

    changed initCalcLegs to reload page when leg won

--- app/javascript/components/initCalcLegs.js
+++ app/javascript/components/initCalcLegs.js
@@ -4,9 +4,10 @@ const initCalcLegs = () => {
   scores.forEach((score) => {
     // console.log(score)
     if (score.innerText === "0") {
-      const id = score.dataset["id"];
-      const leg = document.querySelector(`#leg-${id}`);
-      leg.innerText = parseInt(leg.innerText, 10) + 1;
+      // const id = score.dataset["id"];
+      // const leg = document.querySelector(`#leg-${id}`);
+      // leg.innerText = parseInt(leg.innerText, 10) + 1;
+      window.location.reload(true)
     }
   })
 }
